0d61c43142376b1aad3049561ddcdb85cdc5c484
warren-west <teacher.warren.english@gmail.com>
2025-10-08 14:41:21 +0200

    fix: Allowed CORS to prevent CORS errors.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const PORT = process.env.PORT || 3000
@@ -8,6 +9,7 @@ const computersRouter = require('./routes/computers.js')
 const coffeesRouter = require('./routes/coffees.js')
 const guitarsRouter = require('./routes/guitars.js')
 
+app.use(cors())
 app.use(express.json())
 
 // Map routes
